d9b3dcbcadebe4ce0ffb8fcc399bf751787f3101
sunami09 <sunamidasgupta@gmail.com>
2023-02-05 00:00:38 -0800

    meta deta

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Sunami Dasgupta',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: 'Sunami Dasgupta - Portfolio',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
